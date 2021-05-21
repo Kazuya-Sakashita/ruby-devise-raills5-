5509344c259b7f0522f89e8a33fb8e2a0f68dade
Kazuya-Sakashita <kz0508@gmail.com>
2021-05-21 10:08:43 +0900

    サインアップ時のメール送信実装、確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,12 +56,13 @@ Rails.application.configure do
   # mail setting
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => "Gメールアドレス",
-    :password => "Gメールパスワード",
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => "kz0508@gmail.com", #gmailのアドレス
+  :password => "ciycykjlqmdlldhi",        #gmailのパスワード
+  :authentication => 'login',
+}
 end
